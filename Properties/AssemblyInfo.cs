ecd744a60262a0f282f714765794dcedf39b5ee4
J Miller <jwmiller006@outlook.com>
2024-06-01 07:21:15 -0400

    sign the assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8785f2df-6bd5-4243-b8fc-3db04163ca3a")]
